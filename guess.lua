aabe0b4c69d5f9842b8ea85d0f83ce4975d8dc57
Linus Fernandes <linus.fernandes@gmail.com>
2023-04-25 13:44:06 +0530

    feat: add guess.lua

--- guess.lua
+++ guess.lua
@@ -3,17 +3,20 @@ math.randomseed(os.time())
 number = math.random(1,100)
 player = {}
 player.guess = 0
+player.count = 0
 -- print(number) --debug
 while ( player.guess ~= number ) do
   print("Guess a number between 1 and 100")
   player.answer = io.read()
   player.guess = tonumber(player.answer)
+  player.count = player.count + 1
   if ( player.guess > number ) then
     print("Too high")
   elseif ( player.guess < number) then
     print("Too low")
   else
     print("That's right!")
+    print("No. of guesses: " .. player.count)
     os.exit()
   end
 end
