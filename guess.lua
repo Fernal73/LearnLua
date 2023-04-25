a4f41047ebb26810a09295abbcff137f057d4b44
Linus Fernandes <linus.fernandes@gmail.com>
2023-04-25 13:19:06 +0530

    feat: add guess.lua

---
+++ guess.lua
@@ -0,0 +1,19 @@
+#!/usr/bin/env lua
+math.randomseed(os.time())
+number = math.random(1,100)
+player = {}
+player.guess = 0
+-- print(number) --debug
+while ( player.guess ~= number ) do
+  print("Guess a number between 1 and 100")
+  player.answer = io.read()
+  player.guess = tonumber(player.answer)
+  if ( player.guess > number ) then
+    print("Too high")
+  elseif ( player.guess < number) then
+    print("Too low")
+  else
+    print("That's right!")
+    os.exit()
+  end
+end
