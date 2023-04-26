a202cdf452d50d797c015e6623444b2cbc2c1d12
Linus Fernandes <linus.fernandes@gmail.com>
2023-04-26 21:07:36 +0530

    feat: add ini file example

---
+++ main.lua
@@ -0,0 +1,16 @@
+#!/usr/bin/env lua
+package.path = package.path .. ';/data/data/com.termux/files/usr/share/lua/5.1/?.lua'
+inifile = require('inifile')
+-- find home directory
+home = os.getenv('HOME')
+-- detect path separator
+-- returns '/' for Linux and Mac
+-- and '\' for Windows
+d = package.config:sub(1,1)
+-- parse the INI file and
+-- put values into a table called conf
+conf = inifile.parse("config" .. d .. 'myconfig.ini')
+-- print the data for review
+print(conf['example']['name'])
+print(conf['example']['species'])
+print(conf['example']['enabled'])
